16c9c93265ba69616850ea96069bc8e2fd7ba2e7
Eike Meyer <eike.meyer@deptagency.com>
2024-06-13 14:27:09 +0200

    edit introduce exit codes

--- scripts/sample_api_test.sh
+++ scripts/sample_api_test.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
 # test url
-curl -s -o /dev/null -w "%{http_code}" https://www.$1.com; 
+
+# curl to the input paratemeter
+http_status=$(curl -s -o /dev/null -w "%{http_code}" https://www.$1.com; )
 echo $'\n' $?
+
+# Check if the HTTP status code is 200
+if [ "$http_status" -eq 200 ]; then
+    echo "HTTP status is 200"
+    return 0
+else
+    echo "HTTP status is not 200"
+    return 1
+fi
