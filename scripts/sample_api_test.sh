9329010fc3215fa3c13d24e08be7028e8ece092d
Eike Meyer <eike.meyer@deptagency.com>
2024-06-17 14:15:44 +0200

    echo variable content

--- scripts/sample_api_test.sh
+++ scripts/sample_api_test.sh
@@ -5,7 +5,7 @@
 # curl to the input paratemeter
 http_status=$(curl -s -o /dev/null -w "%{http_code}" '$1'; )
 
-echo '$1'
+echo $1
 
 # Check if the HTTP status code is 200
 if [ "$http_status" -eq 200 ]; then
