fed0c9553d31040b5cb6bd4b5f39d86a9a73ffe5
Eike Meyer <eike.meyer@deptagency.com>
2024-06-17 10:44:16 +0200

    add Dockerfile actions and scripts

--- scripts/sample_api_test.sh
+++ scripts/sample_api_test.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 # test url
 
 # curl to the input paratemeter
 http_status=$(curl -s -o /dev/null -w "%{http_code}" https://www.$1.com; )
-echo $'\n' $?
 
 # Check if the HTTP status code is 200
 if [ "$http_status" -eq 200 ]; then
