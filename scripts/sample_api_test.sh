faa5a0f250e49d7d61bc4fdf4b5d30ec4b79ae7f
Eike Meyer <eike.meyer@deptagency.com>
2024-06-17 14:13:03 +0200

    debugging

--- scripts/sample_api_test.sh
+++ scripts/sample_api_test.sh
@@ -3,7 +3,9 @@
 # test url
 
 # curl to the input paratemeter
-http_status=$(curl -s -o /dev/null -w "%{http_code}" https://www.$1.com; )
+http_status=$(curl -s -o /dev/null -w "%{http_code}" '$1'; )
+
+echo '$1'
 
 # Check if the HTTP status code is 200
 if [ "$http_status" -eq 200 ]; then
